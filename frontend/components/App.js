ef22ecafa70d1a7e8c3f81e32ccd022703c1a033
Timothy Renteria <Timothyxrenteria@gmail.com>
2024-04-09 19:14:26 -0700

    created input onchange for new todo

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,23 +5,33 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
+    error: '',
+    newTodoNameInput: ''
   }
+
   fetchAllTodos = () => {
     axios.get(URL)
       .then(res => {
         this.setState({...this.state, todos: res.data.data})
       })
       .catch(err => {
-
+        this.setState({...this.state, error: err.response.data.message})
       })
   }
+
+  onChange = evt => {
+    const { value } = evt.target
+    this.setState({...this.state, newTodoNameInput: value})
+  }
+
   componentDidMount() {
     this.fetchAllTodos()
+
   }
   render() {
     return (
       <div>
-        <div id="error">Error: No error here</div>
+        <div id="error">{this.state.error !== '' ? `Error: ${this.state.error}` : "" }</div>
         <div id="todos">
           <h2>Todos:</h2>
           {
@@ -31,7 +41,7 @@ export default class App extends React.Component {
           }
         </div>
         <form id="todoForm">
-          <input type="text" placeholder='Type a todo'></input>
+          <input onChange={this.onChange} value={this.state.newTodoNameInput} type="text" placeholder='Type a todo'></input>
           <input type="submit"></input>
           <button>Clear Completed</button>
         </form>
