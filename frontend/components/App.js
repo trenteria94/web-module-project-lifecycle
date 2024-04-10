f1ce21e43d8a62edc05ea275827b9baea6c332e8
Timothy Renteria <Timothyxrenteria@gmail.com>
2024-04-10 16:51:42 -0700

    Finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -58,7 +58,7 @@ export default class App extends React.Component {
           })
         })
       })
-      .catch(this.axiosErrorGiven())
+      .catch(this.axiosErrorGiven)
   }
 
   toggleDisplayedCompleted = () => {
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Todo from './Todo'
 export default class TodoList extends React.Component {
   render() {
     return (
@@ -8,7 +8,11 @@ export default class TodoList extends React.Component {
     {
       this.props.todos.reduce((acc, td) => {
         if (this.props.displayCompleted || !td.completed) return acc.concat(
-          <div onClick={this.props.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✅' : ''}</div>
+        <Todo
+        toggleCompleted={this.props.toggleCompleted}
+        todo={td}
+        key={td.id}
+        />
         )
         return acc
       }, [])
